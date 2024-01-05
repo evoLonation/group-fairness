3243284b392c988b92f9d53044352babfa844b5f
evolonation <1838940019@qq.com>
2024-01-06 00:07:32 +0800

    add old5

--- FUEL/dataload.py
+++ FUEL/dataload.py
@@ -302,12 +302,16 @@ def LoadDataset(args, train_rate = 1.0, test_rate = 1.0):
                 create_dataloader(cut_dataset(ConcatDataset([train_loader.dataset, test_loader.dataset]))[1]) 
                 for train_loader, test_loader in zip(client_train_loads, client_test_loads)
             ]
-        elif args.new_trial_method == 'old4':
+        elif args.new_trial_method == 'old4' or args.new_trial_method == 'old5':
             client_whole_datasets = [ConcatDataset([train.dataset, test.dataset]) 
                                     for train, test in zip(client_train_loads, client_test_loads)]
             def compute_indice(len_data):
-                indices_train = np.random.choice(range(int(len_data * 0.7)), int(len_data * 0.35), replace = False)
-                indices_test = np.random.choice(range(int(len_data * 0.7), len_data), int(len_data * 0.15), replace = False)
+                if args.new_trial_method == 'old4':
+                    indices_train = np.random.choice(range(int(len_data * 0.7)), int(len_data * 0.35), replace = False)
+                    indices_test = np.random.choice(range(int(len_data * 0.7), len_data), int(len_data * 0.15), replace = False)
+                else:
+                    indices_train = np.random.choice(range(len_data), int(len_data * 0.35), replace = False)
+                    indices_test = np.random.choice(list(set(range(len_data)) - set(indices_train)), int(len_data * 0.15), replace = False)
                 indices_another = np.random.choice(range(len_data), int(len_data * 0.50), replace = False)
                 return indices_train, indices_test, indices_another
             indice_tuples = [compute_indice(len(dataset)) for dataset in client_whole_datasets]
@@ -347,7 +351,6 @@ def LoadDataset(args, train_rate = 1.0, test_rate = 1.0):
                 rate_test.value = get_rate(test_indice)
                 rate_another.value = get_rate(another_indice)
                 distrib_train.value = get_distribution(train_indice)
-                print(distrib_train.value)
                 distrib_test.value = get_distribution(test_indice)
                 distrib_another.value = get_distribution(another_indice)
                 def handle_inter(value, set1, set2):
